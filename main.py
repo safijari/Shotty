0d8a19a9e2e1e86ce9fb2b54f3fed8e24ec8308f
Jariullah <safijari@isu.edu>
2023-03-15 23:24:27 -0500

    Readme

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import os
 
-# The decky plugin module is located at decky-loader/plugin
-# For easy intellisense checkout the decky-loader code one directory up
-# or add the `decky-loader/plugin` path to `python.analysis.extraPaths` in `.vscode/settings.json`
 import decky_plugin
 from pathlib import Path
 import json
